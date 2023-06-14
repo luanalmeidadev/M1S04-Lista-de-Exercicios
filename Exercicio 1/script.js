4de3b9ba6f12daef349263073c56a7be85be185b
Luan Almeida <luan.rego01@gmail.com>
2023-06-13 23:10:32 -0300

    completion of the first exercise

--- Exercicio 1/script.js
+++ Exercicio 1/script.js
@@ -1,5 +1,19 @@
-let nota1 = prompt("Digite a primeira nota: ");
-let nota2 = 
-let nota3 =
+//função criada para exibir um resultado.
+function showResult(frase) {
+    document.write(frase);
+}
 
-prompt ();
+//função criada para calcular notas.
+function calculateNotes(nota1, nota2, nota3) {
+    var notas = (nota1 + nota2 + nota3) /3;
+    return notas;
+}
+
+    let yourName = prompt("Informe o seu nome");
+    let fistNote = parseInt(prompt(yourName + ", insira sua primeira nota"));
+    let secondNote = parseInt(prompt(yourName + ", insira sua segunda nota"));
+    let thirdNote = parseInt(prompt(yourName + ", insira sua terceira nota"));
+
+    let totalNotes = calculateNotes(fistNote, secondNote, thirdNote);
+
+    showResult(yourName + ", sua média é: " + totalNotes.toFixed(2));
